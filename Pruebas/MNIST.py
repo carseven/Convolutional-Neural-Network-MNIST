afe5135c2778aaad76b3847e4b753ab399d23335
carseven <carles.serra@hotmail.es>
2020-03-26 23:50:28 +0100

    MNIST.py

---
+++ Pruebas/MNIST.py
@@ -0,0 +1,28 @@
+#importamos librerias
+import tensorflow as tf
+from tensorflow import keras
+
+#Cargamos el dataset de MNIST.
+(train_images, train_labels),(test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+#modelo
+model = keras.Sequential([
+                          keras.layers.Flatten(input_shape=(28,28)),
+                          keras.layers.Dense(128, activation=tf.nn.relu),
+                          keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+#Entrenamos el modelo
+model.fit(train_images, train_labels, epochs=5)
+
+#Evaluamos
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('Precision del test: ', test_acc)
+
+#predicciones
+predictions = model.predict(test_images)
+
