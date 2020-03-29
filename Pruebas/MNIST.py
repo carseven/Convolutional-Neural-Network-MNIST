ad760307f9a2634f4ec2536e3fd8641b910d0316
carseven <carles.serra@hotmail.es>
2020-03-29 21:54:19 +0200

    Añadimos dataset.

--- Pruebas/MNIST.py
+++ Pruebas/MNIST.py
@@ -1,28 +1,35 @@
-#importamos librerias
 import tensorflow as tf
 from tensorflow import keras
 
-#Cargamos el dataset de MNIST.
+"Cargamos el dataset de MNIST."
 (train_images, train_labels),(test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+print("Cargamos el dataset de MNIST")
 
-#modelo
+"Observamos nuestro dataset"
+print(train_images.shape)
+print(len(train_labels))
+print(train_labels)
+
+
+
+"modelo"
+
 model = keras.Sequential([
                           keras.layers.Flatten(input_shape=(28,28)),
                           keras.layers.Dense(128, activation=tf.nn.relu),
                           keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
-
+"""
 model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-#Entrenamos el modelo
+"Entrenamos el modelo"
 model.fit(train_images, train_labels, epochs=5)
 
-#Evaluamos
+"Evaluamos"
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 print('Precision del test: ', test_acc)
 
-#predicciones
-predictions = model.predict(test_images)
-
+"Predicciones"
+predictions = model.predict(test_images)"""
