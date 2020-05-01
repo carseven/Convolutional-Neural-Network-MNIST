aa636ac552aba5c0e891f0ffeb0847f5c0650a64
carseven <carles.serra@hotmail.es>
2020-05-01 18:00:39 +0200

    2D conv example. PEP8 format

--- computerVision/dataset/SimpleDatasetLoader.py
+++ computerVision/dataset/SimpleDatasetLoader.py
@@ -4,31 +4,30 @@ import os
 
 
 class SimpleDatasetLoader(object):
-	"""docstring for SimpleDatasetLoader"""
-	def __init__(self, preprocessors=None):
-		self.preporcessors = preporcessors
+    """docstring for SimpleDatasetLoader"""
 
-		if self.preporcessors is None:
-			self.preporcessors = []
+    def __init__(self, preprocessors=None):
+        self.preprocessors = preprocessors
 
-	def load(self, imagePath, verbose=1):
-		data = []
-		labels = []
+        if self.preprocessors is None:
+            self.preprocessors = []
 
-		for (i, imagePath) in enumerate(imagePath):
-			image = cv2.imread(imagePath)
-			label = imagePath.split(os.path.sep)[-2]
+    def load(self, imagePath, verbose=1):
+        data = []
+        labels = []
 
-			if self.preprocessors is not None:
-				for p in self.preprocessors:
-					image = p.preprocess(image)	
+        for (i, imagePath) in enumerate(imagePath):
+            image = cv2.imread(imagePath)
+            label = imagePath.split(os.path.sep)[-2]
 
-			data.append(image)
-			labels.append(label)
+            if self.preprocessors is not None:
+                for p in self.preprocessors:
+                    image = p.preprocess(image)
 
-			if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
-				print("[INFO] processed {}/{}".format(i + 1, len(imagePath)))
+            data.append(image)
+            labels.append(label)
 
-		return (np.array(data), np.array(labels))
+            if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
+                print("[INFO] processed {}/{}".format(i + 1, len(imagePath)))
 
-		
+        return (np.array(data), np.array(labels))
